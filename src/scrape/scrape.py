c28ccc98b8c82b698c049b471796f839c6cfd619
Juuso Meriläinen <juuso.merilainen@gofore.com>
2016-12-02 19:10:16 +0200

    parse data from threads, save as json

--- src/scrape/scrape.py
+++ src/scrape/scrape.py
@@ -1,11 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import time
+import random
 
-BASE_URL = 'http://www.vauva.fi/'
-VAUVA_URL = BASE_URL + 'keskustelu/alue/{subject}?page={page}'
+BASE_URL = 'http://www.vauva.fi'
+VAUVA_URL = BASE_URL + '/keskustelu/alue/{subject}?page={page}'
+
+def get_sleep_time():
+    return random.randrange(100, 221) / 1000
 
 def get_soup(html):
     return BeautifulSoup(html, 'html.parser')
+
+def fetch_as_soup(url):
+    response = requests.get(url)
+    return get_soup(response.text)
 
 def get_threads(page):
     thread_page_response = requests.get(VAUVA_URL.format(subject='aihe_vapaa', page=page))
@@ -19,24 +29,42 @@ def get_threads(page):
         for span in single_thread.find_all('span'):
             span.replaceWith('')
         single_thread = get_soup(str(single_thread))
+
         threads.append({
             'url': BASE_URL + single_thread.a['href'],
             'title': single_thread.a.contents[0],
         })
     return threads
 
-def get_thread_last_page_number(thread_soup):
+def get_page_count(thread_soup):
     last_page_bullet = thread_soup.find('li', {'class': 'pager-last last'})
     if last_page_bullet is not None:
-        return int(last_page_bullet.a.contents[0]) - 1
-    return 0
+        return int(last_page_bullet.a.contents[0])
+    return 1
 
-def get_thread_pages(thread):
-    first_page = requests.get(thread['url'])
-    first_page_soup = get_soup(first_page.text)
-    page_count = get_thread_last_page_number(first_page_soup)
-    print(page_count)
-    return []
+def get_page_content(page):
+    return str(page.find('div', {'class': 'region-main'}))
+
+def get_keijo(thread):
+    keijo = {
+        'title': thread['title'],
+        'url': thread['url'],
+        'pages': [],
+    }
+    first_page_soup = fetch_as_soup(thread['url'])
+    page_count = get_page_count(first_page_soup)
+    keijo['pages'].append(get_page_content(first_page_soup))
+
+    for page_number in range(1, page_count):
+        page_url = thread['url'] + '?page=' + str(page_number)
+        print('fetching ' + page_url)
+        page = fetch_as_soup(page_url)
+        content = get_page_content(page)
+        keijo['pages'].append(content)
+        time.sleep(get_sleep_time())
+
+    return keijo
+
 
 def main():
     page = 0
@@ -47,11 +75,14 @@ def main():
             break
         page += 1
         # TODO
-        if page > 5:
-            break
+        break
 
+    keijos = []
     for thread in threads:
-        thread_pages = get_thread_pages(thread)
+        keijos.append(get_keijo(thread))
+
+    with open('keijos.json', 'w+') as f:
+        f.write(json.dumps(keijos, indent=2))
 
 if __name__ == '__main__':
     main()
