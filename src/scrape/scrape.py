4430e5e21525ea45c88542be9b6c6535ee9c37ce
Juuso Meriläinen <juuso.merilainen@gmail.com>
2016-12-03 00:16:27 +0200

    Renamed variables to more sensible names, cleaned up style

--- src/scrape/scrape.py
+++ src/scrape/scrape.py
@@ -4,43 +4,31 @@ import json
 import time
 import random
 
+
 BASE_URL = 'http://www.vauva.fi'
 VAUVA_URL = BASE_URL + '/keskustelu/alue/{subject}?page={page}'
+
 
 def get_sleep_time():
     return random.randrange(100, 221) / 1000
 
+
 def convert_to_soup(html):
     return BeautifulSoup(html, 'html.parser')
 
+
 def fetch_as_soup(url):
+    print('fetching ' + url)
     response = requests.get(url)
     return convert_to_soup(response.text)
 
-def get_threads(page):
-    thread_page_response = requests.get(VAUVA_URL.format(subject='aihe_vapaa', page=page))
-    if thread_page_response.status_code != 200:
-        return []
-
-    threads = []
-    thread_page_soup = convert_to_soup(thread_page_response.text)
-    for single_thread in thread_page_soup.find_all('span', {'class': 'title'}):
-        # Remove redundant content
-        for span in single_thread.find_all('span'):
-            span.replaceWith('')
-        single_thread = convert_to_soup(str(single_thread))
-
-        threads.append({
-            'url': BASE_URL + single_thread.a['href'],
-            'title': single_thread.a.contents[0],
-        })
-    return threads
 
 def get_page_count(thread_soup):
     last_page_bullet = thread_soup.find('li', {'class': 'pager-last last'})
     if last_page_bullet is not None:
         return int(last_page_bullet.a.contents[0])
     return 1
+
 
 def get_page_content(page):
     main_region = page.find('div', {'class': 'region-main'})
@@ -63,27 +51,50 @@ def get_page_content(page):
 
     return str(main_region)
 
-def get_keijo(thread):
-    keijo = {
+
+def get_threads(page, subject='aihe_vapaa'):
+    thread_list_url = VAUVA_URL.format(subject=subject, page=page)
+    thread_list_response = requests.get(thread_list_url)
+    if thread_list_response.status_code != 200:
+        return []
+
+    threads = []
+    thread_list_soup = convert_to_soup(thread_list_response.text)
+    for single_thread in thread_list_soup.find_all('span', {'class': 'title'}):
+        # Remove redundant content
+        for span in single_thread.find_all('span'):
+            span.replaceWith('')
+        single_thread = convert_to_soup(str(single_thread))
+
+        threads.append({
+            'url': BASE_URL + single_thread.a['href'],
+            'title': single_thread.a.contents[0],
+        })
+
+    return threads
+
+
+def get_thread_contents(thread):
+    thread_contents = {
         'title': thread['title'],
         'url': thread['url'],
         'pages': [],
     }
     first_page_soup = fetch_as_soup(thread['url'])
     content = get_page_content(first_page_soup)
-    keijo['pages'].append(content)
-    
+    thread_contents['pages'].append(content)
+
     page_count = get_page_count(first_page_soup)
 
     for page_number in range(1, page_count):
         page_url = thread['url'] + '?page=' + str(page_number)
-        print('fetching ' + page_url)
         page = fetch_as_soup(page_url)
         content = get_page_content(page)
-        keijo['pages'].append(content)
+        thread_contents['pages'].append(content)
         time.sleep(get_sleep_time())
 
-    return keijo
+    return thread_contents
+
 
 def main():
     page = 0
@@ -96,12 +107,14 @@ def main():
         # TODO
         break
 
-    keijos = []
+    thread_contents = []
     for thread in threads:
-        keijos.append(get_keijo(thread))
+        thread_contents.append(get_thread_contents(thread))
 
-    with open('keijos.json', 'w+') as f:
-        f.write(json.dumps(keijos, indent=2))
+    # TODO write to DB
+    with open('vauva-threads.json', 'w+') as f:
+        f.write(json.dumps(thread_contents, indent=2))
+
 
 if __name__ == '__main__':
     main()
