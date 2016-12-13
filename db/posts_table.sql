6d702ef77629330fb394d83cd1ed6210f913c0a4
rk <risto.kuustera@gmail.com>
2016-12-13 17:19:53 +0200

    Added post number column

--- db/posts_table.sql
+++ db/posts_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE posts
 (
     id UUID     PRIMARY KEY DEFAULT gen_random_uuid(),
     topic_id    INT REFERENCES topics(id),
-    content     TEXT,
+    content     TEXT NOT NULL,
     post_time   timestamp without time zone NOT NULL DEFAULT '1970-01-01 00:00:00'::timestamp without time zone,
-    post_number INT
+    post_number INT NOT NULL
 );
