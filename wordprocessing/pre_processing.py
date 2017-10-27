d53b22057304ad1f9392f81ae51935d611b2cf84
Juuso Meriläinen <juuso.merilainen@gofore.com>
2017-10-27 17:23:25 +0300

    Pre-process full discussion threads and store to db

--- wordprocessing/pre_processing.py
+++ wordprocessing/pre_processing.py
@@ -1,32 +1,34 @@
 import pgdb
 import libvoikko
+from tqdm import tqdm
 from gensim.models.phrases import Phrases, Phraser
 
 _VOIKKO = libvoikko.Voikko('fi')
+
 _POST_QUERY = 'SELECT id, content FROM posts'
 _TOPIC_QUERY = '''SELECT topic_id AS id, string_agg(content, '\n\n') AS content
                   FROM posts
                   GROUP BY topic_id'''
+_INSERT_NORMALIZED = 'INSERT INTO normalized_topics(id, content) VALUES(%d, %s)'
 _BATCH_SIZE = 100
 
 with open('stop_words.txt') as f:
     stop_words = [word.rstrip() for word in f]
 
 def get_rows(db, query):
-    # TODO change loop to while True when actually doing this
-    cursor = db.cursor()
-    cursor.execute(query)
-    for i in range(20):
-        rows = cursor.fetchmany(_BATCH_SIZE)
-        if not rows:
-            break
-        yield from rows
+    with db.cursor() as cursor:
+        cursor.execute(query)
+        while True:
+            rows = cursor.fetchmany(_BATCH_SIZE)
+            if not rows:
+                break
+            yield from rows
 
 def get_topics(db):
-    yield from get_rows(db, _TOPIC_QUERY)
+    yield from tqdm(get_rows(db, _TOPIC_QUERY))
 
 def get_posts(db):
-    yield from get_rows(db, _POST_QUERY)
+    yield from tqdm(get_rows(db, _POST_QUERY))
 
 def get_normalized_sentences(db):
     for post in get_posts(db):
@@ -42,7 +44,7 @@ def normalize(text):
         if not is_word(token):
             continue
         word = lemmatize(token)
-        if word is not None and not is_stop_word(word):
+        if word and not is_stop_word(word):
             words.append(word)
     return words
 
@@ -54,8 +56,9 @@ def is_word(token):
 
 def lemmatize(token):
     # TODO
-    # Check spelling suggestions if the word is unknown?
-    # Ensure these are actually good.
+    # - Check spelling suggestions if the word is unknown?
+    # - Ensure these are actually good.
+    # - Always return something?
     analysis = _VOIKKO.analyze(token.tokenText)
     if analysis:
         return analysis[0]['BASEFORM'].lower()
@@ -63,13 +66,21 @@ def lemmatize(token):
 def is_stop_word(word):
     return word in stop_words
 
-# TODO normalize full posts (or topics), apply trigram model, store somewhere
-db = pgdb.connect(database='vauvafi')
-phrases = Phrases(get_normalized_sentences(db))
-bigram = Phraser(phrases)
-trigram = Phrases(bigram[get_normalized_sentences(db)])
+def build_phrase_model(stream):
+    return Phraser(Phrases(stream))
 
-for topic in get_topics(db):
-    normalized_topic = trigram[normalize(topic.content)]
-    print(' '.join(normalized_topic))
+def pre_process_topic(topic, bigram, trigram):
+    return ' '.join(trigram[bigram[normalize(topic)]])
 
+def pre_process_topics():
+    with pgdb.connect(database='vauvafi') as db:
+        bigram = build_phrase_model(get_normalized_sentences(db))
+        bigram.save('bigram_model')
+        trigram = build_phrase_model(bigram[get_normalized_sentences(db)])
+        trigram.save('trigram_model')
+        with db.cursor() as cursor:
+            cursor.executemany(_INSERT_NORMALIZED, (
+                (topic.id, pre_process(topic.content, bigram, trigram))
+                for topic in get_topics(db)
+            ))
+        db.commit()
